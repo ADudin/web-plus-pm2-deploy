17e24ae9e466c8550484e0c4b22afb404008b75c
ADudin <gwaeron27@yandex.ru>
2024-02-15 15:23:16 +0300

    feat: remove crush-test for backend after review

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -13,11 +13,11 @@ import { validateUserBody, validateAuthentication } from '../middlewares/validat
 const router = Router();
 
 // убрать после ревью
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// router.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 
 router.post('/signup', validateUserBody, createUser);
 router.post('/signin', validateAuthentication, login);
